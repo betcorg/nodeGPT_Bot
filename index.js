40ead82f64c6936fc8f6e2d1a40d279856e46f84
H. Franc <betcorg@gmail.com>
2023-06-05 01:32:39 -0600

    Agrego integración con ChatGPT

--- index.js
+++ index.js
@@ -1,19 +1,73 @@
-const { Configuration, OpenAIApi } = require("openai"); 
-const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY, }); 
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
+const model = "gpt-3.5-turbo";
+const temp = 0;
+const max_tokens = 30;
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const client = new Client({
+    authStrategy: new LocalAuth(),
+});
 
-async function getResponse() {
 
-    const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{
-            "role": "user",
-            "content": "¿Quién fue Cristobal Colón?",
-        }],
-        temperature: 0,
-        max_tokens: 100,
-    }); 
-    console.log(response.data.choices[0].message.content);
-}
+const waClient = function () {
 
-getResponse();
+    client.on('qr', (qr) => {
+        qrcode.generate(qr, { small: true });
+    });
+    client.on('ready', () => {
+        console.log('Client is ready!');
+    });
+    client.initialize();
+};
+waClient();
+
+
+client.on("message", (msg) => {
+    const prompt = msg.body;
+
+    async function listener() {
+        console.log(prompt);
+        // getResponse(prompt);
+        const response = await getResponse(prompt);
+        client.sendMessage(msg.from, response);
+    };
+    listener();
+});
+
+
+
+async function getResponse(prompt) {
+    const params = {
+        model: model,
+        messages: [
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
+        temperature: temp,
+        max_tokens: max_tokens,
+    };
+
+    const response = await openai.createChatCompletion(params);
+    const content = response.data.choices[0].message.content;
+    // console.log(content);
+    return content;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
